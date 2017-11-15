89c34c534c03d77aae54dd6857172bac39f6ca2c
Beamsy <jake.dbeams@gmail.com>
2017-11-15 15:13:23 +0000

    Fix to ActionBar Size

--- app/src/main/java/uk/co/beamsy/bookzap/bookzap/BookZap.java
+++ app/src/main/java/uk/co/beamsy/bookzap/bookzap/BookZap.java
@@ -4,17 +4,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookZap extends AppCompatActivity {
     private static String[] bookTitles = {"Leviathan's Wake", "Abbadon's Gate", "Absolution Gap"};
     private DrawerLayout drawerLayout;
     private ListView drawerList;
     private ActionBarDrawerToggle drawerToggle;
+    private RecyclerView recyclerView;
+    private BookCardAdaptor bookAdaptor;
+    private List<Book> bookList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +53,28 @@ public class BookZap extends AppCompatActivity {
                 invalidateOptionsMenu();
             }
         };
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        bookList = new ArrayList<>();
+        bookAdaptor = new BookCardAdaptor(this, bookList);
+
+        RecyclerView.LayoutManager _layoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(_layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(bookAdaptor);
+
+        prepareData();
+
+
+    }
+
+    private void prepareData(){
+        Author a = new Author("Brandon", "Sanderson", 0);
+        Book b = new Book("Oathbringer", a, 0);
+        a = new Author("James", "Corey", 1);
+        b = new Book("Leviathan Wakes", a, 0);
+
+        bookAdaptor.notifyDataSetChanged();
     }
 
     @Override
